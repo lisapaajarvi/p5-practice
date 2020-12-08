2d3df8edc84e150fc3c49ab1fb2f58a9781869e1
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2020-12-08 15:22:45 +0100

    added movement to square

--- sketch.js
+++ sketch.js
@@ -10,15 +10,17 @@ function setup() {
   
   let value = 0;
   let x = 0;
+  let xpos = 25;
   function draw() {
     fill(value);
-    rect(25, 250, 50, 50);
+    rect(xpos, 250, 50, 50);
 
     ellipse(x, height/2, 20, 20);
     x = x + 1;
 }
 
 function mouseClicked() {
+  xpos+=50;
   if (value === 0) {
     value = 255;
   } else {
